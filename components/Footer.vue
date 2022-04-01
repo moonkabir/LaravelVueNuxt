b1f9534c4ccc9dcc2b36ac3d677fcca3e235de51
MoonKabir <moonkabir4@gmail.com>
2022-04-01 11:11:02 +0600

    class 5 hero slider done

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,6 +1,82 @@
 <template>
 	<div>
-        footer
+        <div class="container">
+			<div class="flex mx-4 mb-10">
+				<div class="w-1/4 px-4 flex items-center">
+					<div class="min-w-max mr-4">
+						<img width="80" src="~/assets/img/customer-support.png" alt="">
+					</div>
+					<div class="w-full">
+						<h3 class="text-xl font-normal mb-2">24 Customer Support</h3>
+						Contact us 24 hours
+					</div>
+				</div>
+				<div class="w-1/4 px-4 flex items-center">
+					<div class="min-w-max mr-4">
+						<img width="80" src="~/assets/img/authentic-products.png" alt="">
+					</div>
+					<div class="w-full">
+						<h3 class="text-xl font-normal mb-2">Authentic Products</h3>
+						Contact us 24 hours
+					</div>
+				</div>
+				<div class="w-1/4 px-4 flex items-center">
+					<div class="min-w-max mr-4">
+						<img width="80" src="~/assets/img/secure-payment.png" alt="">
+					</div>
+					<div class="w-full">
+						<h3 class="text-xl font-normal mb-2">Secure Payment</h3>
+						Contact us 24 hours
+					</div>
+				</div>
+				<div class="w-1/4 px-4 flex items-center">
+					<div class="min-w-max mr-4">
+						<img width="80" src="~/assets/img/best-price.png" alt="">
+					</div>
+					<div class="w-full">
+						<h3 class="text-xl font-normal mb-2">Best Price & Offers</h3>
+						Contact us 24 hours
+					</div>
+				</div>
+			</div>
+
+		</div>
+		<div class="bg-gray-200 py-20">
+			<div class="container">
+				<div class="flex">
+					<div class="w-3/5 pr-10">
+						<Logo/>
+						<p class="my-8">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vero dolor voluptatem tempore corrupti sint autem omnis, iusto voluptate unde animi at voluptas optio cupiditate! Numquam sit possimus exercitationem beatae architecto.</p>
+						<div class="flex">
+							<a href="#" class="mr-4"><img src="~/assets/img/playstore.png" alt=""></a>
+							<a href="#"><img src="~/assets/img/appstore.png" alt=""></a>
+						</div>
+					</div>
+					<div class="w-1/5 pl-6">
+						<h3 class="text-2xl mb-6">About Us</h3>
+						<ul class="leading-loose">
+							<li><nuxt-link to="/">About Karte</nuxt-link></li>
+							<li><nuxt-link to="/">Contact</nuxt-link></li>
+							<li><nuxt-link to="/">Career</nuxt-link></li>
+							<li><nuxt-link to="/">Terms & Conditions</nuxt-link></li>
+							<li><nuxt-link to="/">Category</nuxt-link></li>
+							
+						</ul>
+					</div>
+					<div class="w-1/5 pl-6">
+						<h3 class="text-2xl mb-6">Info</h3>
+						<ul class="leading-loose">
+							<li><nuxt-link to="/">Information</nuxt-link></li>
+							<li><nuxt-link to="/">Shipping</nuxt-link></li>
+							<li><nuxt-link to="/">Payment</nuxt-link></li>
+							<li><nuxt-link to="/">Return</nuxt-link></li>
+							<li><nuxt-link to="/">Blog</nuxt-link></li>
+							
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
 	</div>
 </template>
 
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,60 @@
 <template>
 	<div>
-		This is index page
+		<!-- <vue-slick-carousel :arrows="true" :dots="true">
+			<div>1</div>
+			<div>2</div>
+			<div>3</div>
+			<div>4</div>
+		</vue-slick-carousel> -->
+		<div class="p-8">
+			<vue-slick-carousel :arrows="false" :dots="true" class="hero-slide">
+			<div class="slide-bg rounded-2xl pt-8">
+				<div class="container">
+					<div class="flex items-center">
+						<div class="w-1/2 mr-6 font-size-22">
+							<p class="bs-dark-green-color font-size-32 mb-4">Save up 30% off</p>
+							<h2 class="text-7xl font-bold mb-6 text-gray-800">Bengal Vegetable farm Organic 100%</h2>
+							<p class="mb-8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab a ducimus est harum deleniti qui sed.</p>
+							<nuxt-link to="/" class="bs-button">Shop Now</nuxt-link>
+						</div>
+						<div class="w-1/2 flex justify-end">
+						<img width="550" src="~/assets/img/hero.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="slide-bg rounded-2xl pt-8">
+				<div class="container">
+					<div class="flex items-center">
+						<div class="w-1/2 mr-6 font-size-22">
+							<p class="bs-dark-green-color font-size-32 mb-4">Save up 40% off</p>
+							<h2 class="text-7xl font-bold mb-6 text-gray-800">Bengal Vegetable farm Organic 100%</h2>
+							<p class="mb-8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab a ducimus est harum deleniti qui sed.</p>
+							<nuxt-link to="/" class="bs-button">Shop Now</nuxt-link>
+						</div>
+						<div class="w-1/2 flex justify-end">
+						<img width="550" src="~/assets/img/hero.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+			<div class="slide-bg rounded-2xl pt-8">
+				<div class="container">
+					<div class="flex items-center">
+						<div class="w-1/2 mr-6 font-size-22">
+							<p class="bs-dark-green-color font-size-32 mb-4">Save up 50% off</p>
+							<h2 class="text-7xl font-bold mb-6 text-gray-800">Bengal Vegetable farm Organic 100%</h2>
+							<p class="mb-8">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ab a ducimus est harum deleniti qui sed.</p>
+							<nuxt-link to="/" class="bs-button">Shop Now</nuxt-link>
+						</div>
+						<div class="w-1/2 flex justify-end">
+						<img width="550" src="~/assets/img/hero.png" alt="">
+						</div>
+					</div>
+				</div>
+			</div>
+			</vue-slick-carousel>
+		</div>
 	</div>
 </template>
 
